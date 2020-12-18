cebd64b15742a2d9b024d27d2d510fb34d963c15
Francis Gulotta <wizard@roborooter.com>
2020-12-18 17:05:16 -0500

    v2 but messed up docs?
    Got versions working right

---
+++ docusaurus.config.js
@@ -0,0 +1,161 @@
+module.exports = {
+  title: 'Node Serialport',
+  tagline: 'The tagline of my site',
+  url: 'https://serialport.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'serialport', // Usually your GitHub org/user name.
+  projectName: 'website', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'My Site',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          type: 'docsVersionDropdown',
+          position: 'right',
+          dropdownActiveClassDisabled: true,
+          dropdownItemsAfter: [
+            {
+              to: '/versions',
+              label: 'All versions',
+            },
+          ],
+        },
+        {
+          href: 'https://github.com/serialport/node-serialport',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'About',
+              to: 'docs/',
+            },
+            {
+              label: 'Code of Conduct',
+              to: 'docs/code-of-conduct',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/serialport/node-serialport',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Francis Gulotta, Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/serialport/serialport-website/edit/master/website/',
+          /**
+           * Whether to display the author who last updated the doc.
+           */
+          showLastUpdateAuthor: true,
+          /**
+           * Whether to display the last date the doc was updated.
+           */
+          showLastUpdateTime: true,
+          /**
+           * Skip the next release docs when versioning is enabled.
+           * This will not generate HTML files in the production build for documents
+           * in `/docs/next` directory, only versioned docs.
+           */
+          includeCurrentVersion: false,
+          /**
+           * The last version is the one we navigate to in priority on versioned sites
+           * It is the one displayed by default in docs navbar items
+           * By default, the last version is the first one to appear in versions.json
+           * By default, the last version is at the "root" (docs have path=/docs/myDoc)
+           * Note: it is possible to configure the path and label of the last version
+           * Tip: using lastVersion: 'current' make sense in many cases
+           */
+          // lastVersion: undefined,
+          /**
+           * The docusaurus versioning defaults don't make sense for all projects
+           * This gives the ability customize the label and path of each version
+           * You may not like that default version
+           */
+          versions: {
+            /*
+            Example configuration:
+            current: {
+              label: 'Android SDK v2.0.0 (WIP)',
+              path: 'android-2.0.0',
+            },
+            '1.0.0': {
+              label: 'Android SDK v1.0.0',
+              path: 'android-1.0.0',
+            },
+            */
+          },
+          /**
+           * Sometimes you only want to include a subset of all available versions.
+           * Tip: limit to 2 or 3 versions to improve startup and build time in dev and deploy previews
+           */
+          onlyIncludeVersions: undefined, // ex: ["current", "1.0.0", "2.0.0"]
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/serialport/serialport-website/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ src/pages/versions.js
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Link from '@docusaurus/Link';
+import Layout from '@theme/Layout';
+
+import {useVersions, useLatestVersion} from '@theme/hooks/useDocs';
+
+function Version() {
+  const {siteConfig} = useDocusaurusContext();
+  const versions = useVersions();
+  const latestVersion = useLatestVersion();
+  const pastVersions = versions.filter(version => version !== latestVersion);
+  const repoUrl = `https://github.com/${siteConfig.organizationName}/${siteConfig.projectName}`;
+
+  console.log({ versions, latestVersion, pastVersions, versions, pastVersions})
+  return (
+    <Layout
+      title="Versions"
+      permalink="/versions"
+      description="SerialPort Versions page listing all documented versions">
+      <main className="container margin-vert--lg">
+        <h1>SerialPort documentation versions</h1>
+
+        <div className="margin-bottom--lg">
+          <h3 id="latest">Next version (Unreleased)</h3>
+          <p>
+            Here you can find the documentation for work-in-process unreleased
+            version.
+          </p>
+          <table>
+            <tbody>
+              <tr>
+                <th>{latestVersion.label}</th>
+                <td>
+                  <Link to={latestVersion.path}>Documentation</Link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        {pastVersions.length > 0 && (
+          <div className="margin-bottom--lg">
+            <h3 id="archive">Past versions (Not maintained anymore)</h3>
+            <p>
+              Here you can find documentation for previous versions of
+              Docusaurus.
+            </p>
+            <table>
+              <tbody>
+                {pastVersions.map((version) => (
+                  <tr key={version.name}>
+                    <th>{version.label}</th>
+                    <td>
+                      <Link to={version.path}>Documentation</Link>
+                    </td>
+                    {/* <td>
+                      <a href={`${repoUrl}/releases/tag/v${version.name}`}>
+                        Release Notes
+                      </a>
+                    </td> */}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </main>
+    </Layout>
+  );
+}
+
+export default Version;
